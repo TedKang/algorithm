e31e1857d9c04722e065707638124ff8343c184f
Ted <Ted@DESKTOP-Q0CR313>
2018-11-30 14:52:53 +0900

    problem2869

--- Algorithm/src/Main.java
+++ Algorithm/src/Main.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
 
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class Main {		
+	
+	public static void main(String[] args) {		
+		Scanner sc = new Scanner(System.in);		
+		long tmp[] = Utils.getLongArray(sc.nextLine().split(" "));
+		
+		Problem2869 solve = new Problem2869(tmp[0], tmp[1], tmp[2]);
+		System.out.println(solve.getRes());
+	}	
 }
